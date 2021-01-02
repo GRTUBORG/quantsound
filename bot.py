201ef6e8f2e9c0e8d84b57848a8ff2498b7c5e7b
Dennis <blinhic850@mail.ru>
2021-01-02 23:31:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,7 @@ from discord.ext.commands import Bot
 from discord.utils import get
 from discord import FFmpegPCMAudio
 
-os.system('whereis ffmpeg')
-os.system('which ffmpeg')
-
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'False'}
+YDL_OPTIONS = {'format': 'worstaudio/best', 'noplaylist': 'True', 'simulate': 'True', 'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 youtube_dl.utils.bug_reports_message = lambda: ''
