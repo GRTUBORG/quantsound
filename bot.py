c0f42b594ed294053a125bf2f530c94c5d0bc438
Dennis <blinhic850@mail.ru>
2021-01-03 00:52:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,18 +18,25 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 intents = discord.Intents.all()
 Bot = commands.Bot(command_prefix = ["/"], intents = discord.Intents.all())
 
-@Bot.command()
+@Bot.command(aliases = ['p'])
 async def play(ctx, *, arg):
     voice_channel = ctx.message.author.voice.channel
     vc = await voice_channel.connect()
     if vc.is_playing():
         await ctx.send(f'{ctx.message.author.mention}, музыка уже проигрывается.')
-
     else:
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(arg, download=False)
-        URL = info['entries'][0]['formats'][0]['url']
-        vc.play(discord.FFmpegPCMAudio(executable="/app/vendor/ffmpeg/ffmpeg", source=URL, **FFMPEG_OPTIONS))
+        correct_url = url[:8]
+        correct_url1 = 'https://'
+        correct_search = 'ytsearch:' + url
+        if correct_url != correct_url1:
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(arg, download = False)
+                URL = info['entries'][0]['formats'][0]['url']
+        else: 
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(arg, download = False)
+                URL = info['formats'][0]['url']
+        vc.play(discord.FFmpegPCMAudio(executable = "/app/vendor/ffmpeg/ffmpeg", source = URL, **FFMPEG_OPTIONS))
 
         while vc.is_playing():
             await sleep(1)
