981bcb211dc3cf0524b9e18ca1bebbdf4e06c241
Dennis <blinhic850@mail.ru>
2021-01-15 22:57:36 +0300

    Update quantsound.py

--- quantsound.py
+++ quantsound.py
@@ -22,7 +22,7 @@ help_message = (':flag_ru:\n'
                 '\n\n'
                 ':flag_us:\n'
                 '**[West coast](http://the-radio.ru/radio/pvpjamz-west-coast-r637)**')
-update = '14.01.21'
+update = '15.01.21'
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 intents = discord.Intents.all()
