a6111598c123193c9ce084765d91351c394c336d
Dennis <blinhic850@mail.ru>
2021-02-05 00:57:14 +0300

    Update quantsound.py

--- quantsound.py
+++ quantsound.py
@@ -30,7 +30,7 @@ update = os.environ.get('update')
 token = os.environ.get('bot_token')
 
 help_message = (':radio:\n'
-                '**• [Europe +](https://europaplus.ru)** (different), \n**• [Radio Energy](https://www.energyfm.ru)** (different), \n**• [West coast](http://the-radio.ru/radio/pvpjamz-west-coast-r637)** (rap), \n**• [CORE RADIO](https://coreradio.ru)** (rock), \n**• [Phonk](https://101.ru/radio/user/865080)** (memphis rap), \n**• [Radio Record](https://www.radiorecord.ru)**(different),'
+                '**• [Europe +](https://europaplus.ru)** (different), \n**• [Radio Energy](https://www.energyfm.ru)** (different), \n**• [West coast](http://the-radio.ru/radio/pvpjamz-west-coast-r637)** (rap), \n**• [CORE RADIO](https://coreradio.ru)** (rock), \n**• [Phonk](https://101.ru/radio/user/865080)** (memphis rap), \n**• [Record](https://www.radiorecord.ru)** (different),'
                 '\n**• [Record Deep](https://www.radiorecord.ru/station/deep)** (deep house), \n**• [Record Pirate Station](https://www.radiorecord.ru)** (drum and bass), \n**• [Record Black Rap](https://www.radiorecord.ru)** (rap), '
                 '\n**• [Record Rock](https://www.radiorecord.ru)** (rock), \n**• [Record Trap](https://www.radiorecord.ru)** (trap), \n**• [Record Dubstep](https://www.radiorecord.ru)** (dubstep)')
 
@@ -488,7 +488,7 @@ async def help_radio(ctx):
     embed = discord.Embed(title = 'List of available radio stations', description = help_message)
     embed.set_footer(text = "supports by quantsound")
     message = await ctx.send(embed = embed)
-    await asyncio.sleep(15)
+    await asyncio.sleep(45)
     await message.delete()
     
   
